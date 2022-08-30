56b92b22b53b34c451bc54895767ff2c3360913d
joobasb <aromaajoonas@gmail.com>
2022-08-30 13:47:16 +0300

    First commit, testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  //define varialbes
+  const [bottles, setBottles] = useState(0)
+  const [litres, setLitres] = useState(0)
+  const [grams, setGrams] = useState(0)
+  const [weight, setWeight] = useState(0)
+  const [gender, setGender] = useState(0)
+  const [burning, setBurning] = useState(0)
+  const [time, setTime] = useState(0)
+  const [result, setResult] = useState(0)
+
+  function calculate(e){
+    e.preventDefault()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form>
+      <h1>Alcometer</h1><br/>
+      <div>
+      <label>Weight </label>
+      <input type="number" value={weight} onChange={e => setWeight(e.target.value)} />
+      </div>
+      <div>
+        <label>Bottles </label>
+        <input type="number" value={bottles} onChange={e => setBottles(e.target.value)} />
+      </div>
+      <div>
+        <label>Time </label>
+        <input type="number" value={time} onChange={e => setTime(e.target.value)} />
+      </div>
+      <div>
+        <label>Gender </label>
+        <input type="radio" value="male" defaultChecked onChange={e => setGender(e.target.value)} /><label>Male</label>
+        <input type="radio" value="female" onChange={e => setGender(e.target.value)} /><label>Female</label>
+      </div>
+      <div><br/>
+        <output>{result}</output>
+      </div>
+      <br/>
+      <button>Calculate</button>
+      </form>
+    </>
+
+
   );
 }
 
