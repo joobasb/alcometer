218dd918769e8561db2b3ff3d91f7aecfe2be780
joobasb <aromaajoonas@gmail.com>
2022-08-30 16:26:45 +0300

    Working app, minor style adjusts

--- src/App.js
+++ src/App.js
@@ -4,23 +4,39 @@ import { useState } from 'react'
 
 function App() {
 
-  //define varialbes
-  const [bottles, setBottles] = useState(0)
-  const [litres, setLitres] = useState(0)
-  const [grams, setGrams] = useState(0)
-  const [weight, setWeight] = useState(0)
-  const [gender, setGender] = useState(0)
-  const [burning, setBurning] = useState(0)
-  const [time, setTime] = useState(0)
-  const [result, setResult] = useState(0)
+  //define variables
+  const [bottles, setBottles] = useState()
+  const [weight, setWeight] = useState(80)
+  const [gender, setGender] = useState('male')
+  const [time, setTime] = useState(1)
+  const [result, setResult] = useState()
 
   function calculate(e){
     e.preventDefault()
+
+    const litres = bottles * 0.33
+    const grams = litres * 8 * 4.5
+    const burning = weight / 10
+    const gramsLeft = grams - (burning * time)
+    let result2;
+
+    if (gender === "male") {
+      result2 = gramsLeft / (weight * 0.7)
+      setResult(result2.toFixed(2))
+    } else {
+      result2 = gramsLeft / (weight * 0.6)
+      setResult(result2.toFixed(2))
+    } 
+    if (result2 < 0 ){
+      result2 = 0
+      setResult(result2)
+    } 
   }
+
 
   return (
     <>
-    <form>
+    <form onSubmit={calculate}>
       <h1>Alcometer</h1><br/>
       <div>
       <label>Weight </label>
@@ -28,19 +44,45 @@ function App() {
       </div>
       <div>
         <label>Bottles </label>
-        <input type="number" value={bottles} onChange={e => setBottles(e.target.value)} />
+        <select type="number" value={bottles} onChange={e => setBottles(e.target.value)}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+        </select>
       </div>
       <div>
         <label>Time </label>
-        <input type="number" value={time} onChange={e => setTime(e.target.value)} />
+        <select type="number" value={time} onChange={e => setTime(e.target.value)}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+        </select>
       </div>
       <div>
         <label>Gender </label>
-        <input type="radio" value="male" defaultChecked onChange={e => setGender(e.target.value)} /><label>Male</label>
-        <input type="radio" value="female" onChange={e => setGender(e.target.value)} /><label>Female</label>
+        <input type="radio" name="gender" value="male" defaultChecked onChange={e => setGender(e.target.value)} /><label>Male</label>
+        <input type="radio" name="gender" value="female" onChange={e => setGender(e.target.value)} /><label>Female</label>
       </div>
       <div><br/>
-        <output>{result}</output>
+        <output> {result}</output>
       </div>
       <br/>
       <button>Calculate</button>
