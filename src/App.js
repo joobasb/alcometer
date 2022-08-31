34ce83472c3af93c0f8aeeb0858c4f1f17fb0413
joobasb <aromaajoonas@gmail.com>
2022-08-31 11:44:40 +0300

    Fixed NaN return, clarified var name, minor adds

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react'
 function App() {
 
   //define variables
-  const [bottles, setBottles] = useState()
+  const [bottles, setBottles] = useState(1)
   const [weight, setWeight] = useState(80)
   const [gender, setGender] = useState('male')
   const [time, setTime] = useState(1)
@@ -18,26 +18,28 @@ function App() {
     const grams = litres * 8 * 4.5
     const burning = weight / 10
     const gramsLeft = grams - (burning * time)
-    let result2;
+    let totalResult; 
 
     if (gender === "male") {
-      result2 = gramsLeft / (weight * 0.7)
-      setResult(result2.toFixed(2))
+      totalResult = gramsLeft / (weight * 0.7)
+      setResult(totalResult.toFixed(2))
     } else {
-      result2 = gramsLeft / (weight * 0.6)
-      setResult(result2.toFixed(2))
+      totalResult = gramsLeft / (weight * 0.6)
+      setResult(totalResult.toFixed(2))
     } 
-    if (result2 < 0 ){
-      result2 = 0
-      setResult(result2)
-    } 
+    if (totalResult < 0 ){
+      totalResult = 0
+      setResult(totalResult)
+    }
   }
 
 
   return (
     <>
     <form onSubmit={calculate}>
-      <h1>Alcometer</h1><br/>
+      <h1>Alcometer</h1>
+      <p>Calculate your blood alcohol level</p>
+      <br/>
       <div>
       <label>Weight </label>
       <input type="number" value={weight} onChange={e => setWeight(e.target.value)} />
@@ -81,10 +83,9 @@ function App() {
         <input type="radio" name="gender" value="male" defaultChecked onChange={e => setGender(e.target.value)} /><label>Male</label>
         <input type="radio" name="gender" value="female" onChange={e => setGender(e.target.value)} /><label>Female</label>
       </div>
-      <div><br/>
+      <div>
         <output> {result}</output>
       </div>
-      <br/>
       <button>Calculate</button>
       </form>
     </>
